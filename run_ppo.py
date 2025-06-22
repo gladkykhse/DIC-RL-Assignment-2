6089acb20bd0c4c8a74369bb7793f95e46d09b28
Thijmen Schinkelshoek <thijmen.schinkelshoek@gmail.com>
2025-06-22 14:26:26 +0200

    Update run_ppo script

--- run_ppo.py
+++ run_ppo.py
@@ -1,8 +1,6 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import torch
-import random
-import numpy as np
 
 try:
     from world import Environment, Grid
@@ -17,6 +15,14 @@ except ModuleNotFoundError:
     from world import Environment
 
 
+START_POSITIONS = {
+    "small_grid.npy": (1, 2),
+    "small_grid_2.npy": (1, 2),
+    "custom_medium_grid_1.npy": (4, 6),
+    "custom_medium_grid_2.npy": (4, 6),
+}
+
+
 def parse_args():
     p = ArgumentParser(description="PPO Agent Evaluator.")
     p.add_argument("GRID", type=Path, nargs="+", help="Paths to the grid file to use.")
@@ -24,11 +30,12 @@ def parse_args():
     p.add_argument("--sigma", type=float, default=0)
     p.add_argument("--fps", type=int, default=30)
     p.add_argument("--iter", type=int, default=500)
-    p.add_argument("--random_seed", type=int, default=43)
+    p.add_argument("--random_seed", type=int, default=0)
     p.add_argument("--agent_start_pos", type=str)
     return p.parse_args()
 
-def load_agent(ckpt_path: Path) -> PPOAgent:
+def load_agent(ckpt_path, random_seed) -> PPOAgent:
+    torch.manual_seed(random_seed)
     ckpt = torch.load(ckpt_path, map_location="cpu")
     grid = Grid.load_grid(Path(ckpt["grid_fp"])).cells
     agent = PPOAgent(
@@ -43,20 +50,18 @@ def load_agent(ckpt_path: Path) -> PPOAgent:
 
 
 def main(grid_paths, no_gui, max_steps, fps, sigma, random_seed, agent_start_pos):
-    if agent_start_pos:
-        agent_start_pos = tuple(map(int, agent_start_pos.split(",")))
-    
-    # set random seeds for reproducibility
-    random.seed(random_seed)
-    np.random.seed(random_seed)
-    torch.manual_seed(random_seed)
-
     for grid_fp in grid_paths:
         ckpt_path = Path(f"models/PPO_{grid_fp.stem}.pt")
         if not ckpt_path.exists():
             raise FileNotFoundError(f"Checkpoint not found: {ckpt_path}")
 
-        agent = load_agent(ckpt_path)
+        agent = load_agent(ckpt_path, random_seed)
+
+        if agent_start_pos:
+            start_pos = tuple(map(int, agent_start_pos.split(",")))
+        else:
+            start_pos = START_POSITIONS.get(grid_fp.name, None)
+
 
         Environment.evaluate_agent(
             grid_fp,
@@ -64,7 +69,7 @@ def main(grid_paths, no_gui, max_steps, fps, sigma, random_seed, agent_start_pos
             max_steps=max_steps,
             sigma=sigma,
             show_images=not no_gui,
-            agent_start_pos=agent_start_pos,
+            agent_start_pos=start_pos,
             random_seed=random_seed,
         )
 
